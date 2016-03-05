bd475bed9f63104c8ff85507b3ed1db27db4be4c
kennethbigler <kennethbigler@gmail.com>
2016-03-04 20:01:18 -0800

    Ken begins angular

---
+++ README.md
@@ -0,0 +1,2 @@
+# lambda2pi
+This is a project by Scott Sarsfield and Kenneth Bigler for a Formal Methods class where we convert a statement in Lambda Calculus to Pi Calculus.
---
+++ app/components/home/HomeController.js
@@ -0,0 +1,6 @@
+/*global $, console, app, $scope, $location*/
+
+app.controller('HomeController', ['$scope', 'HomeService', '$Lexer', function ($scope, HomeService, $Lexer) {
+    "use strict";
+    
+}]);
